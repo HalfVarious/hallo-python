a5a9b3aaa6c44dd3fcd969aa9f6c33b673fb083d
HalfVarious <99067555@mydavinci.nl>
2021-09-16 13:01:48 +0200

    Speelhaldag/Feestlunch

--- DeSpeelhalDag.py
+++ DeSpeelhalDag.py
@@ -2,9 +2,10 @@
 
 aantalpersonen = 3
 prijsticket = 7.45
-prijsminuten = 0.37
+prijsminuten = 0.370
 
 totaalprijsticket = aantalpersonen * prijsticket
 totaalprijsgameseat = aantalpersonen * prijsminuten * 9
+totaalprijs = totaalprijsticket + totaalprijsgameseat
 
-print(totaalprijsgameseat + totaalprijsticket)
+print("Dit geweldige dagje-uit met 3 mensen in de Speelhal met 45 minuten VR kost je maar €" + str(totaalprijs))
--- Feestlunch.py
+++ Feestlunch.py
@@ -3,6 +3,7 @@
 AantalCroissantjes = 17
 AantalStokbroden = 2 
 AantalKorting = 3
+
 PrijsCroissantjes = 0.39
 PrijsStokbroden = 2.78
 PrijsKorting = -0.50
@@ -11,4 +12,6 @@ TotaalprijsCroissantjes = AantalCroissantjes * PrijsCroissantjes
 TotaalprijsStokbroden = AantalStokbroden * PrijsStokbroden
 Korting = AantalKorting * PrijsKorting
 
-print(TotaalprijsCroissantjes + TotaalprijsStokbroden + Korting)
+totaalprijs = TotaalprijsCroissantjes + TotaalprijsStokbroden + Korting
+
+print("De feestlucnh kost je bij de bakker €" + str(totaalprijs) + " voor de 17 croissantjes en de 2 stokboden als de 3 kortingsbonnen nog geldig zijn")
