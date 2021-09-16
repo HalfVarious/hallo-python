7672816f458541554bc377a1e7d0923739c331cf
HalfVarious <99067555@mydavinci.nl>
2021-09-16 10:05:24 +0200

    Speelhaldag/Feestlunch

---
+++ DeSpeelhalDag.py
@@ -0,0 +1,10 @@
+# de Speelhaldag
+
+aantalpersonen = 3
+prijsticket = 7.45
+prijsminuten = 0.37
+
+totaalprijsticket = aantalpersonen * prijsticket
+totaalprijsgameseat = aantalpersonen * prijsminuten * 9
+
+print(totaalprijsgameseat + totaalprijsticket)
---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+\# Feestlunch
+
+AantalCroissantjes = 17
+AantalStokbroden = 2 
+AantalKorting = 3
+PrijsCroissantjes = 0,39
+PrijsStokbroden = 2,78
+PrijsKorting = -0,50
+
+TotaalprijsCroissantjes = (AantalCroissantjes * PrijsCroissantjes)
+TotaalprijsStokbroden = (AantalStokbroden * PrijsStokbroden)
+Korting = (AantalKorting * PrijsKorting)
+
+print(TotaalprijsCroissantjes + TotaalprijsStokbroden - Korting)
